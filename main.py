36ddf1415b6736125d09b5133d15c54eb3519a28
DieievDaniel <120338641+DieievDaniel@users.noreply.github.com>
2023-04-10 14:00:35 +0300

    Create main

---
+++ main.py
@@ -0,0 +1,40 @@
+def get_price_change(product_name):
+    import datetime
+
+    # Відкриваємо файл з даними про товари
+    with open('data.txt', 'r') as file:
+        # Читаємо кожний рядок файла
+        lines = file.readlines()
+
+    # Знаходимо дату, яка була місяць тому
+    one_month_ago = datetime.datetime.now() - datetime.timedelta(days=30)
+
+    # Створюємо словник для зберігання даних про ціну товару
+    prices = {}
+
+    # Проходимося по кожному рядку файла
+    for line in lines:
+        # Розділяємо рядок на назву товару, дату та ціну
+        name, date_str, price_str = line.strip().split(',')
+
+        # Переводимо дату в об'єкт datetime
+        date = datetime.datetime.strptime(date_str, '%Y-%m-%d')
+
+        # Якщо дата більше, ніж місяць тому, і назва товару відповідає вхідному параметру
+        if date > one_month_ago and name == product_name:
+            # Зберігаємо ціну товару у словнику
+            prices[date] = float(price_str)
+
+    # Якщо немає записів про ціну товару за останній місяць
+    if not prices:
+        return "Немає записів про ціну товару за останній місяць"
+
+    # Знаходимо першу та останню ціну товару за останній місяць
+    first_price = min(prices.values())
+    last_price = max(prices.values())
+
+    # Обчислюємо зміну ціни
+    price_change = last_price - first_price
+
+    # Повертаємо результат
+    return f"Ціна товару {product_name} змінилась на {price_change} за останній місяць"
